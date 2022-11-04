7525d25c9e73ebbda8000505a1c9d6a320edf92e
Erick Acevedo <erac_17@alu.uabcs.mx>
2022-11-04 17:07:11 -0600

    feat: cleanning omf

--- .config/qtile/.config/omf/key_bindings.fish
+++ .config/qtile/.config/omf/key_bindings.fish
@@ -1,12 +1,23 @@
 # Just clear the commandline on control-c
-# las comillas son para comandos del editor y sin las comillas para ejecuciones de comandos pero ocn atajos
+# las comillas son para comandos del editor y sin las comillas para ejecuciones de comandos pero con atajos
+
+# execute
 bind \cc 'commandline -r ""'
-bind \en nvim
 bind \cy redo
+bind \ch history
+bind \ct 'tree -C'
+
+# go to something or things which control can not handle it
+
+#ctrl can't handle it
 bind \e\[3\;5~ kill-word
-bind \eh history
+
+# go to something
+bind \eh htop
 bind \er ranger
 bind \et tmux
-bind \ct 'tree -C'
+bind \en nvim
+bind \el lvim
+
 # bind \t forward-char
 
