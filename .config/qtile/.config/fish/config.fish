f3eb9675bf6a67a679a153120e3a73f981ab1775
Erick Acevedo <erac_17@alu.uabcs.mx>
2023-10-29 11:14:45 -0600

    feat: commands updates
    most important -> secure uninstalls

--- .config/qtile/.config/fish/config.fish
+++ .config/qtile/.config/fish/config.fish
@@ -1,9 +1,13 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-# terminal
-    alias l "lsd"
+# terminal commands
+    alias l "lsd -h"
+    alias la "lsd -ah"
+    alias ll "lsd -lh"
     alias nv "nvim"
-    alias p "pacman"
+
+    # alias xampp "sudo xampp"
+    # alias maven "~/maven/bin/mvn"#el comando de maven es mvn 
 
 # tmux alias
     alias tl "tmux ls"
@@ -13,18 +17,31 @@ if status is-interactive
 # lazygit    
     alias lg "lazygit"
 
-# bashrc
-    alias update='sudo pacman -Syyu'
-    alias cleanup='sudo pacman -Rns $(pacman -Qtdq)'
+# updates
+    alias update-db='sudo pacman -Sy'
+    alias update-system='sudo pacman -Syyu'
     alias update-fc='sudo fc-cache -fv' # to font install 
+
+# fixes
     alias sysfailed="systemctl list-units --failed"
-    alias bls="betterlockscreen -u /usr/share/backgrounds/arcolinux/"
-    alias grubconfig="sudo $EDITOR /etc/default/grub"
-    alias grubupdate="sudo grub-mkconfig -o /boot/grub/grub.cfg"
-    alias grubootconfig="sudo $EDITOR /boot/grub/grub.cfg"
     alias keyfix="/usr/local/bin/arcolinux-fix-pacman-databases-and-keys"
     alias fix-pacman-conf="/usr/local/bin/arcolinux-fix-pacman-conf"
     alias fix-pacman-keyserver="/usr/local/bin/arcolinux-fix-pacman-gpg-conf"
 
+# grubconfig
+    alias grubconfig="sudo $EDITOR /etc/default/grub"
+    alias grubupdate="sudo grub-mkconfig -o /boot/grub/grub.cfg"
+    alias grubootconfig="sudo $EDITOR /boot/grub/grub.cfg"
+
+# backgrounds edits
+  alias bls="betterlockscreen -u /usr/share/backgrounds/arcolinux/"
+
+# install / uninstall
+    alias ys "yay -S" # to use AUR repositories and as user not root
+  
+  # to use official pacman repositorires as root
+    alias pins "sudo pacman -S"
+    alias punins "sudo pacman -Rns"
+    alias cleanup='sudo pacman -Rns $(pacman -Qtdq)'
 
 end
